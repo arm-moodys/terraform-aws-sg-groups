c37e35ec765cd8e19331d6aeea9a49091fac493c
arm-moodys <178335655+arm-moodys@users.noreply.github.com>
2024-10-21 03:40:37 +0000

    refactor: Simplify outbound security group rule for ALB
    The outbound security group rule for the Application Load Balancer (ALB) has been simplified by removing unnecessary port restrictions. The rule now allows all IP protocols and all ports to be accessed from any IP address (0.0.0.0/0).

--- main.tf
+++ main.tf
@@ -24,10 +24,7 @@ resource "aws_security_group_rule" "sg_allow_inbound" {
 
 resource "aws_vpc_security_group_egress_rule" "alb_sg_allow_outbound" {
   depends_on                   = [aws_security_group.main]
-
   security_group_id            = aws_security_group.main.id
-  ip_protocol                  = "tcp"
-  from_port                    = "-1"
-  to_port                      = "-1"
+  ip_protocol                  = "-1"
   cidr_ipv4                    = "0.0.0.0/0"
 }
