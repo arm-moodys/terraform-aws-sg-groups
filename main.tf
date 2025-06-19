bea32165e6b033560884dac11d61475b2607e245
arm-moodys <178335655+arm-moodys@users.noreply.github.com>
2025-06-19 18:56:36 +0000

    refactor: Consolidate security group rules into a single resource

--- main.tf
+++ main.tf
@@ -3,28 +3,27 @@ resource "aws_security_group" "main" {
   description = "${var.name} security group"
   vpc_id      = var.vpc_id
 
+  dynamic "ingress" {
+    for_each = var.inbound_ports
+    content {
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = var.ingress_cidr_ipv4
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = merge(
     {
       "Name" = "${var.name}"
     },
     var.tags
   )
-}
-
-resource "aws_security_group_rule" "sg_allow_inbound" {
-  for_each          = var.inbound_ports
-  type              = "ingress"
-  security_group_id = aws_security_group.main.id
-
-  from_port   = each.value.port
-  to_port     = each.value.port
-  protocol    = "tcp"
-  cidr_blocks = var.ingress_cidr_ipv4
-}
-
-resource "aws_vpc_security_group_egress_rule" "alb_sg_allow_outbound" {
-  depends_on        = [aws_security_group.main]
-  security_group_id = aws_security_group.main.id
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
 }
